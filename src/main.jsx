7264bedbac74900069caa7c3665540b5311be596
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2023-12-08 04:12:28 -0500

    feat(useRef): add useRef example

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
-
-import './index.css'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-// import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterWithCustomHook /> */}
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
-    <MultipleCustomHooks />
+    {/* <MultipleCustomHooks /> */}
+    <FocusScreen />
   </React.StrictMode>,
 )
