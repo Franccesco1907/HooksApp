284d894f123e7c7e8fdaec9864fd5ac53681c9cf
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2024-01-05 09:53:24 -0500

    feat(useReducer): add useReducer app

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,51 +1,34 @@
-import { useReducer } from "react"
-import { todoReducer } from "./todoReducer"
-
-const initialState = [
-  {
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    desc: 'Aprender Angular',
-    done: false,
-  }
-]
+import { useTodos } from "../hooks/useTodos"
+import { TodoAdd } from "./TodoAdd"
+import { TodoList } from "./TodoList"
 
 export const TodoApp = () => {
-  const [state, dispatch] = useReducer(todoReducer, initialState)
+  const {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  } = useTodos()
+
   return (
     <>
-      <h1>TodoApp (10), <small>pendientes: 2</small></h1>
+      <h1>TodoApp ({todosCount}), <small>pendientes: {pendingTodosCount}</small></h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group">
-            <li className="list-group-item d-flex justify-content-between">
-              <span className="align-self-center">Item 1</span>
-              <button className="btn btn-danger">Borrar</button>
-            </li>
-          </ul>
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Qué hay que hacer?"
-              className="form-control"
-            />
-            <button
-              type="submit"
-              className="btn btn-outline-primary mt-1"
-            >
-              Agregar
-            </button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
 
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,8 +1,13 @@
 export const todoReducer = (initialState, action) => {
   switch (action.type) {
-    case 'ABC':
-      throw new Error('Action.type = ABC no está implementado');
-
+    // case 'ABC':
+    //   throw new Error('Action.type = ABC no está implementado');
+    case '[TODO] Add Todo':
+      return [...initialState, action.payload];
+    case '[TODO] Remove Todo':
+      return initialState.filter(todo => todo.id !== action.payload);
+    case '[TODO] Toggle Todo':
+      return initialState.map(todo => (todo.id === action.payload) ? { ...todo, done: !todo.done } : todo);
     default:
       return initialState;
   }
